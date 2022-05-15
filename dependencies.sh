7c130b64149bf8988cdceee25942853c24cd008f
nandankalia <99944363+nandankalia@users.noreply.github.com>
2022-05-15 16:07:55 +0530

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-pip python3-dev nginx
+sudo pip3 install virtualenv
+mkdir ~/projectdir
+cd ~/projectdir
+virtualenv env
+source env/bin/activate
+sudo apt update
