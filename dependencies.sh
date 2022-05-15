eb71a816dd4153b29810ab3ed2ec374b5b901e3f
nandankalia <99944363+nandankalia@users.noreply.github.com>
2022-05-15 16:36:07 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -3,8 +3,7 @@
 sudo apt update
 sudo apt install python3-pip python3-dev nginx
 sudo pip3 install virtualenv
-mkdir ~/projectdir
-cd ~/projectdir
 virtualenv env
 source env/bin/activate
+pip install django gunicorn
 sudo apt update
